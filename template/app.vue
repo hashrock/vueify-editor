25ea896d2d8884e2d0a8c64bae2061bd8a4f3af1
hashrock <hashedrock@gmail.com>
2015-12-02 18:16:06 +0900

    Update sample

--- template/app.vue
+++ template/app.vue
@@ -2,17 +2,25 @@
   .red {
     color: #f00;
   }
+  li{
+  	color: blue;
+  }
 </style>
 
 <template>
   <h1 class="red">{{msg}}</h1>
+  <p>{{msg}}</p>
+  <ul>
+  	<li v-for="item in items">{{item}} です</li>
+  </ul>
 </template>
 
 <script>
   module.exports = {
     data: function () {
       return {
-        msg: 'Hello world!'
+        msg: 'Hello world!',
+        items: ["first", "second", "third"]
       }
     }
   }
